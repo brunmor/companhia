65f8ed253402e0541933b163b6d9e36562c207bd
Gabriel Menezes <gabrielufmscc@gmail.com>
2018-11-24 10:25:36 +0000

    Adicionando outros atributos da classe Voo

--- backend/src/main/java/com/grupo14/companhia_aerea/dominio/voo/Voo.java
+++ backend/src/main/java/com/grupo14/companhia_aerea/dominio/voo/Voo.java
@@ -3,17 +3,63 @@ package com.grupo14.companhia_aerea.dominio.voo;
 import com.grupo14.companhia_aerea.dominio.EntidadeBase;
 
 import javax.persistence.Entity;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Voo extends EntidadeBase {
-    private String cidadeDeOrigem;
 
-    public Voo(String cidadeDeOrigem) {
+    private String cidadeDeOrigem;
+    private String cidadeDeDestino;
+    private LocalDate dataDeIda;
+    private LocalDate dataDeVolta;
+    private List<Assento> assentos;
+
+    public Voo(String cidadeDeOrigem, String cidadeDeDestino, LocalDate dataDeIda, LocalDate dataDeVolta) {
 
         this.cidadeDeOrigem = cidadeDeOrigem;
+
+        this.cidadeDeDestino = cidadeDeDestino;
+
+        this.dataDeIda = dataDeIda;
+
+        this.dataDeVolta = dataDeVolta;
+
+        this.assentos = new ArrayList<>();
+
+        for (int i = 0; i < 60; i++){
+            assentos.add(new Assento(false, i));
+        }
     }
 
     public String getCidadeDeOrigem() {
         return this.cidadeDeOrigem;
+    }
+
+    public String getCidadeDeDestino() {
+        return this.cidadeDeDestino;
+    }
+
+    public LocalDate getDataDeIda() {
+        return this.dataDeIda;
+    }
+
+    public LocalDate getDataDeVolta() {
+        return this.dataDeVolta;
+    }
+
+    public List<Assento> getAssentos() {
+        return this.assentos;
+    }
+
+    private class Assento {
+        boolean ocupado;
+        int codigoDoAssento;
+
+        Assento(boolean ocupado, int codigoDoAssento){
+            this.ocupado = ocupado;
+            this.codigoDoAssento = codigoDoAssento;
+        }
     }
 }
